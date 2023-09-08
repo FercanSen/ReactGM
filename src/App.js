e84d9e0c7cfbf7d221b4650676edef900b095642
FercanSen <fercansen@gmail.com>
2023-09-08 15:56:33 +0300

    Add SearhBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import "./App.css";
 import Counter from "./components/Counter";
 
+import { useState } from "react";
+import SearchBar from "./components/SearchBar";
+
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = (query) => {
+    setSearchResults([query]);
+  };
+
   return (
     <div>
-      <Counter></Counter>
+      <Counter />
+      <div>
+        <h1>Search App</h1>
+        <SearchBar initialSearchQuery="" onSearch={handleSearch} />
+        <ul>
+          {searchResults.map((result, index) => (
+            <li key={index}>{result}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+const SearchBar = ({ initialSearchQuery, onSearch }) => {
+  const [searchQuery, setSearchQuery] = useState(initialSearchQuery);
+
+  const handleInputChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(searchQuery);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      onSearch(searchQuery);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchQuery}
+        onChange={handleInputChange}
+        onKeyPress={handleKeyPress}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default SearchBar;
