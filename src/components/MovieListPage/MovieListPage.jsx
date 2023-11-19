4157c531e9ff53301008160b2a35dba35bdded19
FercanSen <fercansen@gmail.com>
2023-11-19 07:54:03 +0300

    Update the selected movie when a movie is selected

--- src/components/MovieListPage/MovieListPage.jsx
+++ src/components/MovieListPage/MovieListPage.jsx
@@ -6,7 +6,7 @@ import "./MovieListPage.scss";
 
 function MovieListPage() {
   const [searchQuery, setSearchQuery] = useState("");
-  const [sortCriterion, setSortCriterion] = useState(""); // You can set a default sort criterion if needed
+  const [sortCriterion, setSortCriterion] = useState("");
   const [activeGenre, setActiveGenre] = useState("");
   const [movieList, setMovieList] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
@@ -16,7 +16,6 @@ function MovieListPage() {
       try {
         const response = await axios.get("http://localhost:4000/movies");
         setMovieList(response.data.data);
-        console.log("Movie List: ");
       } catch (error) {
         console.error("Error fetching movies:", error);
       }
@@ -24,20 +23,27 @@ function MovieListPage() {
 
     fetchData();
   }, []);
-  console.log(movieList);
+
+  const handleMovieClick = (movie) => {
+    setSelectedMovie(movie);
+    console.log("Selected movie:", movie);
+  };
 
   return (
-    <div className="movie-grid">
-      {movieList.map((movie) => (
-        <MovieTile
-          key={movie.id}
-          movieName={movie.title}
-          url={movie.poster_path}
-          releaseYear={movie.release_date.split("-")[0]}
-          genreList={movie.genres.join(", ")}
-        />
-      ))}
-    </div>
+    <>
+      <div className="movie-grid">
+        {movieList.map((movie) => (
+          <MovieTile
+            key={movie.id}
+            movieName={movie.title}
+            url={movie.poster_path}
+            releaseYear={movie.release_date.split("-")[0]}
+            genreList={movie.genres.join(", ")}
+            onClick={() => handleMovieClick(movie)}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
